06bcca6729807c15ab0815609be43683a23ab895
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2024-01-31 11:39:31 +0100

    Add the completed Signaling Server .NET repository

---
+++ ServerSideProject/SignalingServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using WebSocketSharp.Server;
+
+Console.WriteLine("Starting server application");
+
+// Bootstrap the application
+var wssv = new WebSocketServer(8080);
+wssv.AddWebSocketService<SendToOthersService>("/");
+
+// Start the server
+wssv.Start();
+Console.WriteLine("WebSocket Server Running");
+
+// Stop the server when a key is pressed in the console
+Console.WriteLine("Press any key to stop the server...");
+Console.ReadKey(true);
+wssv.Stop();
---
+++ ServerSideProject/SignalingServer/SendToOthersService.cs
@@ -0,0 +1,26 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class SendToOthersService : WebSocketBehavior
+{
+    protected override void OnOpen()
+    {
+        // Log that a new connection was opened
+        var session = Sessions.Sessions.First(s => s.ID == ID);
+        Console.WriteLine("Connection opened with: " + session.ID);
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine("Received message: " + e.Data);
+
+        // Send the received message to all clients except the sender
+        foreach (var id in Sessions.ActiveIDs)
+        {
+            if (id != ID) // ID is the identifier of the current session
+            {
+                Sessions.SendTo(e.Data, id);
+            }
+        }
+    }
+}
